f028ccccccc014a91d7fcb92575e41dbe921d0bb
taxiniort <jbernelas@gmail.com>
2025-11-04 13:38:32 +0100

    Add files via upload

--- constantes.js
+++ constantes.js
@@ -20,12 +20,9 @@ const CONSTANTES = {
 };
 
 const LISTE_AIRE_METRO = [
-"Nantes",
-"Bordeaux",
-"Paris",
-"Rennes",
-"92, 93 et 94",
-"...",
+"Nantes, Bordeaux, Paris, Rennes...",
+"Dép. 92, 93 et 94",
+"+++",
 "34 Clinique St Jean Sud de France 34270 St Jean de Vedas 340024314",
 "34 Clinique du Parc 34057 Castelnau le Lez 340780667",
 "31 Clinique Croix du sud et SSR attaché 31445 Quint Fonsegrives 310026927",
